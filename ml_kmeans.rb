943dabdbda0f3b74f73b8e3aa7c186a0582c7963
Kuo-Ming Lin <ilovekalvar@gmail.com>
2016-05-20 01:19:05 +0800

    Added random choice method.

--- ml_kmeans.rb
+++ ml_kmeans.rb
@@ -34,36 +34,19 @@ class MLKMeans
 		@centers << MLKmeansCenter.new(features, identifier)
 	end
 
-	def choose_center(pick_number = 2, is_random = true)
-
+	def random_choose_centers(pick_number = 2)
 		patterns_count  = @patterns.count
-		if patterns_count < pick_number
-			pick_number = patterns_count
+		if pick_number <= 0
+			random_picker = Random.new
+			pick_number   = random_picker.rand(0...patterns_count) # 等同 rand( 0 ~ (patterns_count-1) )
+		elsif patterns_count < pick_number
+			pick_number   = patterns_count
 		end
 
 		# Random choose centers from patterns
-		if is_random
-			random_picker   = Random.new
-			patterns_count -= 1
-			chunk_length  	= patterns_count / pick_number
-			remain_length	= patterns_count % pick_number # 剩餘的 Chunk 長度
-			max_value	  	= 0
-			min_value	  	= 0
-			# 下面寫法等同 (i=0; i<pick_number; i++)
-			for i in 0...pick_number
-				# 如果是最後一個區段，就多加上剩餘的 Chunk 長度以讓全部的 Patterns 都能夠有機會被選擇到
-				max_value  += (i == pick_number - 1) ? chunk_length + remain_length : chunk_length
-				# Picking [min, max-1] that random number
-				random_index = random_picker.rand(min_value..max_value)
-				@centers << @patterns[random_index]
-				min_value	 = max_value + 1
-			end
-		else
-			# Waiting for implementation an algorithm of choice.
-			# TODO:
-		end
-
+		@centers << @patterns.sample(pick_number)
 	end
+
 
 end
 
